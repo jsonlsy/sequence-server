8736de07d43eae60c14654e05029925b805a7d8e
Jason Lee Sui Yuen <jsonlsy@gmail.com>
2020-08-30 16:34:20 +1000

    Added turns

--- src/game.js
+++ src/game.js
@@ -10,7 +10,7 @@ class Game {
     this.paused = false;
     this.players = {}; // { socketId: { color: } }
     this.playerCardsMap = {}; // { socketId: [] }
-    this.removedPlayersCards = [];
+    this.removedPlayers = []; // [ { card:, turn: }]
     this.turns = [];
     this.currentTurn = 0;
     this.board = new Board();
@@ -27,13 +27,14 @@ class Game {
     Object.keys(this.players).forEach((playerId) => {
       // TODO: calculate number of cards based on number of players
       this.playerCardsMap[playerId] = this.deck.draw(5);
+      this.turns.push(playerId);
     });
   }
 
   addPlayer(playerId) {
     // TODO: maximum number of players
 
-    if (this.started && !this.paused) return;
+    if (this.started && !this.removedPlayers.length) return;
     let color;
 
     if (Object.keys(this.players).length % 2 === 0) {
@@ -44,8 +45,10 @@ class Game {
     this.players[playerId] = { color };
 
     if (this.paused) {
-      this.playerCardsMap[playerId] = this.removedPlayersCards.shift();
-      if (!this.removedPlayersCards.length) this.paused = false;
+      const removedPlayer = this.removedPlayers.shift();
+      this.playerCardsMap[playerId] = removedPlayer.cards;
+      this.turns[removedPlayer.turn] = playerId;
+      if (!this.removedPlayers.length) this.paused = false;
     }
   }
 
@@ -56,8 +59,12 @@ class Game {
 
     if (this.started) {
       this.paused = true;
-      this.removedPlayersCards.push(this.playerCardsMap[playerId]);
+      this.removedPlayers.push({
+        cards: this.playerCardsMap[playerId],
+        turn: this.turns.indexOf(playerId),
+      });
       delete this.playerCardsMap[playerId];
+      delete this.turns[playerId];
     }
   }
 
@@ -81,6 +88,7 @@ class Game {
       }
     });
     this.deck.discard(cardPlayed);
+    this.currentTurn = (this.currentTurn + 1) % Object.keys(this.players).length;
   }
 
   state() {
@@ -92,6 +100,7 @@ class Game {
         started: this.started,
         paused: this.paused,
       },
+      turn: this.turns[this.currentTurn],
     };
   }
 
